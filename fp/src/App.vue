304f7e99eea7be397aee05aec87a3528e6879e09
repo <dragondark48@gmail.com>
2021-06-27 18:13:54 +0300

    lesson1

---
+++ fp/src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" :data="computedDate" />
+    <Calculator />
+    <button @click="onClick">Click me</button>/ {{ count }}
+    <button @click="count = 1 + 2">Sum</button>
+    <button @click="onClick2(1, 4)">Click me2</button>
+    {{ message }}
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import Calculator from "./components/Calculator.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+    Calculator,
+  },
+  data() {
+    return {
+      count: 0,
+      currentDate: null,
+      message: "Hello VUE",
+    };
+  },
+  methods: {
+    onClick() {
+      this.count += 1;
+    },
+    onClick2(param1, param2) {
+      this.count = param1 + param2;
+    },
+  },
+  computed: {
+    computedDate() {
+      return this.currentDate || new Date();
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ fp/src/components/Calculator.vue
@@ -0,0 +1,67 @@
+<template>
+  <div id="app">
+    <input type="number" v-model.number="operand1" placeholder="operand1" />
+    <input
+      type="number"
+      v-model.number="operand2"
+      placeholder="operand2"
+      @blur="sendData(operand2)"
+    />
+    ={{ sum }}
+
+    <div class="keyboard">
+      <button v-on:click="sum = operand1 + operand2">+</button>
+      <button v-on:click="sum = operand1 - operand2">-</button>
+      <button @click="mult">*</button>
+      <button @click="div(operand1, operand2)">/</button>
+      <button @click="divInteger(operand1, operand2)">/floor</button>
+    </div>
+    {{ powSum }}
+    {{ powWithOperand }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Calculator",
+  data() {
+    return {
+      text: "Some string",
+      operand1: 0,
+      operand2: 0,
+      sum: 0,
+    };
+  },
+  watch: {
+    sum: function (newValue, oldValue) {
+      this.console.log(newValue, oldValue);
+    },
+  },
+
+  methods: {
+    mult() {
+      this.sum = this.operand1 * this.operand2;
+    },
+    div(op1, op2) {
+      this.sum = op1 / op2;
+    },
+    divInteger(op1, op2) {
+      this.sum = parseInt(op1 / op2);
+    },
+    sendData() {
+      console.log("Send Data");
+    },
+  },
+  computed: {
+    powWithOperand() {
+      return Math.pow(this.operand1, this.operand2);
+    },
+    powSum() {
+      return Math.pow(this.sum, 4);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
